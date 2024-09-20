7c49410724e5208f621b257153200b4411a1fae7
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-20 21:53:25 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  get ("/", {:controller => "home", :action => "homepage"} )
+  get ("//dice/2/6", {:controller => "two_six", :action => "roll_two_six"} )
+  get ("/dice/2/10", {:controller => "two_ten", :action => "roll_two_ten"} )
+  get ("/dice/1/20", {:controller => "one_twenty", :action => "roll_one_twenty"} ) 
+  get ("/dice/5/4", {:controller => "five_four", :action => "roll_five_four"} ) 
+
 end
