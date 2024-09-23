b0b40ece36d009b9254975bf84d622db0974acdc
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-23 21:09:27 +0000

    Change var, add end tag, controller

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,19 +1,14 @@
 class RandomController < ApplicationController
-
   def throw_random
-
-    @random_dice=rand(1..100)
-    @random_sides=rand(1..100)
-
-    @dice= params.fetch("dice")
-    @number_of_sides = params.fetch("number_of_sides")
+    @number_of_dice = params.fetch("dice").to_i
+    @random_sides = params.fetch("number_of_sides").to_i
 
     @rolls = []
-    @random_dice.times do
+    @number_of_dice.times do
       die = rand(1..@random_sides)
       @rolls.push(die)
     end
+
     render({:template => "templates/throw_random"})
   end
-
 end
