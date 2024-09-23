ddc66e2414ac8235d7178a2ae07c62d40f29335a
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-09-23 17:17:19 +0000

    Add random dice and random sides variable

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -2,6 +2,9 @@ class RandomController < ApplicationController
 
   def throw_random
 
+    @random_dice=rand(1..100)
+    @random_sides=rand(1..100)
+
     @dice= params.fetch("dice")
     @number_of_sides = params.fetch("number_of_sides")
     
